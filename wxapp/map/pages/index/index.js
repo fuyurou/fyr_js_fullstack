c36a69d8b5236eadadd66de3d63785f0dd2fd816
fuyurou <741873409@qq.com>
2018-05-06 19:13:26 +0800

    微信小程序

---
+++ wxapp/map/pages/index/index.js
@@ -0,0 +1,45 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  // 数据绑定
+  data: {
+    latitude: 39.914769,
+    longitude: 116.404556,
+    scale: 15,
+    // controls:
+  },
+  onReady: function() {
+    console.log('ready');
+    // 小程序提供了获取经纬度的方法
+    wx.getLocation({
+      type: 'gcj02',
+      success: (res) => {
+        console.log(res.latitude,res.longitude);
+        this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          scale: 18,
+        })
+      }
+    })
+    // 数据初始化， 数据会随着生命周期或者用户请求发生改变 绑定着数据的界面也要随着改变，自动刷新界面 setData 一个API
+    // this指向page 先显示地图 晚点显示control
+    this.setData({
+      controls: [{
+        id: 1,
+        iconPath: '/images/location.png',
+        position: {
+          left: 20,
+          top: 200,
+          width: 50,
+          height: 50,
+        }
+      }]
+    })
+  },
+  onLoad: function() {
+    console.log('load');
+  }
+})
